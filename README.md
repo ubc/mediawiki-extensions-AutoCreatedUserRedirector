34aeb9aac75bef8e38e2b645f17fb6c66f3bd047
Clarence Ho <clarence.ho@ubc.ca>
2020-05-26 11:34:36 -0700

    Cosmetic changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mediawiki-extensions-AutoCreatedUserRedirector
 
+When a user login mediawiki via external authentication (e.g. LDAP), a local user account is created automatically.  This extension will listen to account auto-creation event and redirect the user to a specific page.
+
+## Setup
+
+In `LocalSettings.php`, set the variable `$wgAutoCreatedUserRedirect` to the wiki page name (e.g. `Main_Page`).
+
--- includes/Hooks.php
+++ includes/Hooks.php
@@ -8,25 +8,25 @@ class Hooks {
     const SESSION_KEY = 'AUTO_CREATED_NEW_USER_CREATED';
 
     /**
-	 * Remember if a new user is autocreated
+     * Remember if a new user is autocreated
      *
-	 * @param User $user
-	 * @param bool $autocreated
-	 */
-	public static function onLocalUserCreated( $user, $autocreated ) {
-		if ( $autocreated ) {
-			$_SESSION[static::SESSION_KEY] = $user->getId();
-		}
-	}
+     * @param User $user
+     * @param bool $autocreated
+     */
+    public static function onLocalUserCreated( $user, $autocreated ) {
+        if ( $autocreated ) {
+            $_SESSION[static::SESSION_KEY] = $user->getId();
+        }
+    }
 
     /**
-	 * While being redirected after login, see if we want to redirect to specific page for new users.
+     * While being redirected after login, see if we want to redirect to specific page for new users.
      *
-	 * @param string &$returnTo page name to redirect to
-	 * @param array &$returnToQuery key value pairs of url parameters
-	 * @param string &$type login redirect condition
-	 * @return true
-	 */
+     * @param string &$returnTo page name to redirect to
+     * @param array &$returnToQuery key value pairs of url parameters
+     * @param string &$type login redirect condition
+     * @return true
+     */
     public static function onPostLoginRedirect( &$returnTo, &$returnToQuery, &$type ) {
         if ( $type == 'successredirect' ) {
             global $wgUser;
